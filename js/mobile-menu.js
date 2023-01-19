a0bf78048b35c6fd733b76abde578537069f4a7b
Anzhela Bazalinska <anzhela.bazalinskaya@gmail.com>
2023-01-19 14:17:05 +0200

    added anchor links

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -15,6 +15,12 @@
   openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
 
+  mobileMenu.querySelectorAll('a').forEach(link => {
+    link.addEventListener('click', () => {
+      mobileMenu.classList.remove('is-open');
+    });
+  });
+
   // Close the mobile menu on wider screens if the device orientation changes
   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
     if (!e.matches) return;
